34d006cae5441fbc3b2a5212a4633a61882d700a
Misaka <52383026+zxy678@users.noreply.github.com>
2020-05-20 19:36:32 +0800

    Update autoload.js

--- live2d_load/autoload.js
+++ live2d_load/autoload.js
@@ -35,7 +35,7 @@ function loadExternalResource(url, type) {
 		initWidget({
 			waifuPath: tips_path + "waifu-tips.min.json",
 			apiPath: "https://live2d.fghrsh.net/api/",
-			//cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
+			//cdnPath: "https://cdn.jsdelivr.net/gh/zxy678/live2d/live2d_api/"
 		});
 	});
 console.log(`
